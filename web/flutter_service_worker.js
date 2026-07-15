d830ea56b615d2b78c3e3f4c42a4e321ebca7dc0
MisaelBohorquez27 <67847941+MisaelBohorquez27@users.noreply.github.com>
2026-07-15 14:35:55 -0500

    fixing code

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -137,7 +137,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "d374ff8330aa6025afb760fe981d4008
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "3e0930469bae12c98bfa16360df61189",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "fa64348fc3e3e32f0fa158cca5b101ee",
+"flutter_bootstrap.js": "ae839ed012e9126323b0fa282b16bb58",
 "icons/amex.svg": "73a76e963ee1289c158c1790b451960c",
 "icons/applepay.svg": "b737182d8ce8513da7c5f4b0b523ae44",
 "icons/discover.svg": "b5c9afa8e9380b9b311bddc8ceb464c5",
@@ -256,7 +256,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "d374ff8330aa6025afb760fe981d4008
 "images/Viceroy/UNIDADES/ESTUDIOS/estudio.png": "f958e0ee792d06a5164ea6badc861dca",
 "index.html": "232b2c71693ca665839dac3cf873f228",
 "/": "232b2c71693ca665839dac3cf873f228",
-"main.dart.js": "af6e6e33e21be933bb90482c01ed5370",
+"main.dart.js": "35895eedf32fde96af7485d33dd1d5bc",
 "manifest.json": "cd1bf5d7f078f23665b0349263b73331",
 "version.json": "7cac1ad24c61678a0a52df0da901933d"};
 // The application shell files that are downloaded before a service worker can
