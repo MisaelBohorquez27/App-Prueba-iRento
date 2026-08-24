00835b31be8847de6ecbb8027e59c52adbc5e304
MisaelBohorquez27 <67847941+MisaelBohorquez27@users.noreply.github.com>
2026-08-24 09:40:41 -0500

    fix

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3111288036"
+    serviceWorkerVersion: "1073198152"
   }
 });
